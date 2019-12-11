e0530bb97e8c82dd14430bf190fba51c63a97e09
黄俊凯 <accounts_5de7f7a95fbf590012b3c7c5@mail.teambition.com>
2019-12-11 17:44:47 +0800

    初始化项目

---
+++ src/layouts/components/navbar/navbarSearchAndPinList.js
@@ -0,0 +1,14 @@
+export default {
+  pages: {
+    key: "title",
+    data: [
+      { title: "首页", url: "/", icon: "HomeIcon", is_bookmarked: false },
+      {
+        title: "用户管理",
+        url: "/page2",
+        icon: "FileIcon",
+        is_bookmarked: false
+      }
+    ]
+  }
+};
